cc8ababbad40bedb13140b9c640f8d3164691e23
Bitajune <beheshtibita@gmail.com>
2020-06-03 10:44:51 -0700

    hoisting practice

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//function declaration uses hoisting
+calculateAge(1990);
+function calculateAge(year) {
+    console.log(2020 - year);
+}
 
+// function expression does not hoist
+let retirement = function (year) {
+    console.log(65 - (2020 - year));
+}
+retirement(1990);
 
+// variables do not hoist
+let age = 30;
+console.log(age);
 
-
-
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    let age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
